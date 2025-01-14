f0ff8c1b23a29dc7a8157b66a29b1fe351bbfde6
Makussu <maximilian@ruhm.info>
2025-01-14 14:50:56 +0100

    init

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      pythonPackages =
+        p: with p; [
+          python-lsp-server
+          pylsp-mypy
+          pyls-isort
+          python-lsp-ruff
+
+          pyqt5
+          pyserial
+        ];
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = [
+          (pkgs.python312.withPackages pythonPackages)
+          pkgs.ruff
+          pkgs.qt5.full
+          pkgs.qt5.wrapQtAppsHook
+        ];
+      };
+    };
+}
